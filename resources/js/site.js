625527cb3ad94de3fbcda9f7d078f6bf9e176332
Joshua Busnelli <info@minemindmedia.com>
2024-01-22 14:17:12 -0700

    minor update

--- resources/js/site.js
+++ resources/js/site.js
@@ -63,12 +63,3 @@ function carousel() {
 
 
 
-
-  function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
